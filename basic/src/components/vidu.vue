ab9a4ec50f5baa5756b8b2d33c4e387bc61fceaf
Ngọc Anh Nguyễn <nguyenngocanhcongviec01@gmail.com>
2022-11-27 11:08:12 +0700

    vue

---
+++ basic/src/components/vidu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Tesst new components</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'vidu'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
